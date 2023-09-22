e66139671bbe52078c6e4323f6f28ef371fb10cb
Bambi <barbiecuedotcom@gmail.com>
2023-09-23 00:07:36 +0800

    fix

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,11 @@
-import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
-import $ from 'jquery';
-import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
+import MobileMenu from "./modules/MobileMenu"
+import RevealOnScroll from "./modules/RevealOnScroll"
+import $ from "jquery"
+import StickyHeader from "./modules/StickyHeader"
+import Modal from "./modules/Modal"
 
-
-var mobileMenu = new MobileMenu();
-new RevealOnScroll($(".trigger-reveal"), "40%");
-new RevealOnScroll($(".trigger-footer"), "95%");
-var stickyHeader = new StickyHeader();
-var modal = new Modal();
+var mobileMenu = new MobileMenu()
+new RevealOnScroll($(".trigger-reveal"), "80%")
+new RevealOnScroll($(".trigger-footer"), "95%")
+var stickyHeader = new StickyHeader()
+var modal = new Modal()
